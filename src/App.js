76df7c9e5c5ba2f42c77f66a02039ddff80faf07
James Stillman <james.stillman@deakin.edu.au>
2018-04-26 19:38:49 +1000

    updated values so renders a list item

--- src/App.js
+++ src/App.js
@@ -5,15 +5,25 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+	<div className="App">
+		<header className="App-header">
+			<img src={logo} className="App-logo" alt="logo" />
+			<h1 className="App-title">Wind At Aireys</h1>
+		</header>
+		<div className="windStation">
+	        <h3>Aireys Inlet</h3>
+			<ul class="windDetails">
+				<li>
+					<span class="label">Direction</span>
+					<span class="value">NW</span>
+				</li>
+				<li>
+					<span class="label">Speed </span>
+					<span class="value">10knts</span>
+				</li>
+			</ul>
+		</div>
+	</div>
     );
   }
 }
